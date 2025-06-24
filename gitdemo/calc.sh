a085caefaf90cc0acae10b2a74efb0d4ebe39a89
trushik <trushik@gmail.com>
2025-06-24 12:37:41 +1000

    this is fun

--- gitdemo/calc.sh
+++ gitdemo/calc.sh
@@ -1,1 +1,1 @@
-I am learning git. It is a lot of fun. I troubleshooted my login issues on git cli.
+I am learning git. It is a lot of fun. I troubleshooted my login issues on git cli. Watching git in action is so much fun.
